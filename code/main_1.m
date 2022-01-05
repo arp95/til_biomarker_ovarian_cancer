d7223f107600417a25f798cb4eb20b7d9d4c64d5
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-01-05 18:56:10 +0700

    Updated files in code/ folde

--- code/main_1.m
+++ code/main_1.m
@@ -28,7 +28,7 @@ draw_option = 0;
 
 
 %% get til features
-for index = 1:5000
+for index = 1:1000
     filename = patches(index).name;
     epi_stroma_mask_path = epi_stroma_masks_dir + filename;
     nuclei_mask_path = nuclei_masks_dir + filename;
