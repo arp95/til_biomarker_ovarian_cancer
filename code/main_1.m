c42d9363c621507cbc6a2a647859de202caf3469
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-03-29 13:53:42 -0400

    Worked on extracting til masks

--- code/main_1.m
+++ code/main_1.m
@@ -28,7 +28,7 @@ draw_option = 0;
 
 
 %% get til features
-for index = 32001:42000
+for index = 7001:12000
     filename = patches(index).name;
     epi_stroma_mask_path = epi_stroma_masks_dir + filename;
     nuclei_mask_path = nuclei_masks_dir + filename;
