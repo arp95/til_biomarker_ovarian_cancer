e0c9ba7dd0e66270fe2ee8b32aff897d1d751513
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-04-03 15:13:04 -0400

    Worked on extracting til masks, added files in code/ folder

--- code/extract_til_masks.py
+++ code/extract_til_masks.py
@@ -12,7 +12,7 @@ print("Header files loaded...")
 
 # get the options selected by user
 til_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_cancer/til_masks/"
-results_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_cancer/til_masks_new/"
+results_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_cancer/til_masks_new_25/"
 patches = glob.glob(til_dir + "*")
 patches = patches[40000:]
 
@@ -23,7 +23,7 @@ for patch in patches:
     image = cv2.imread(image_path)
     image_name = image_path.split("/")[-1]
     
-    for i in range(0, 50):
+    for i in range(0, 25):
         dilated = cv2.dilate(image.copy(), None, iterations=i + 1)
     cv2.imwrite(results_dir + image_name, dilated)
 print("Done!")
