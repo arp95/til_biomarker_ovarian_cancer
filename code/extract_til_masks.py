74eeccd68116f7ae0e875e07a996fb52b2c8c825
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-04-03 12:43:00 -0400

    Worked on extracting til masks, added files in code/ folder

---
+++ code/extract_til_masks.py
@@ -0,0 +1,29 @@
+"""
+Original Author: Arpit Aggarwal
+Description of the file: Script for extracting til masks after running the main til extraction pipeline.
+"""
+
+# header files
+import numpy as np
+import glob
+import cv2
+print("Header files loaded...")
+
+
+# get the options selected by user
+til_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_cancer/til_masks/"
+results_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_cancer/til_masks_new/"
+patches = glob.glob(til_dir + "*")
+patches = patches[:5000]
+
+
+# extract til masks
+for patch in patches:
+    image_path = patch
+    image = cv2.imread(image_path)
+    image_name = image_path.split("/")[-1]
+    
+    for i in range(0, 50):
+        dilated = cv2.dilate(image.copy(), None, iterations=i + 1)
+    cv2.imwrite(results_dir + image_name, dilated)
+print("Done!")
