3b9874326751a1b675154a3f8abb99b52bcdb69c
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-04-11 12:30:28 -0400

    Worked on extracting histoqc masks, modified files in code/ folder

--- code/extract_histoqc_masks.py
+++ code/extract_histoqc_masks.py
@@ -11,11 +11,11 @@ print("Header files loaded...")
 
 
 # get the options selected by user
-patches_dir = "/scratch/users/axa1399/upmc_ovarian_cancer/patches/"
+patches_dir = "/scratch/users/axa1399/upmc_ovarian_cancer/new_patches/"
 histoqc_masks_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/histoqc_mask_output/"
 results_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/upmc_ovarian_cancer/histoqc_masks/"
 patches = glob.glob(patches_dir + "*")
-patches = patches[10000:]
+patches = patches[:4000]
 
 
 # extract histoqc masks
