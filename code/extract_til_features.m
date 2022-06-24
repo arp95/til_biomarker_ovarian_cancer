81e30da36f19d3259f249bcce62e861ea0312cc6
Arpit Aggarwal <arpitdec5@Arpits-MacBook-Pro.local>
2022-06-24 00:07:27 -0400

    Made changes

--- code/extract_til_features.m
+++ code/extract_til_features.m
@@ -1,49 +1,40 @@
 % Author: Arpit Aggarwal
 
-function [features]=extract_til_features(image, nuclei_mask, histoqc_mask, epi_mask, stroma_mask, til_model, draw_option, results_file)
+function [features, flag]=extract_til_features(image, nuclei_mask, histoqc_mask, epi_mask, stroma_mask, til_model, draw_option, results_file, tam_mask)
 %% get til features
 [nuclei_centroids, nuclei_features, nuclei_feature_names, nuclei_bboxes] = get_nuclei_features(image, nuclei_mask);
 nuclei_centroids_rounded = round(nuclei_centroids);
 epi_nuclei = false(length(nuclei_centroids_rounded), 1);
+tam_nuclei = false(length(nuclei_centroids_rounded), 1);
 features = [];
+flag = -1;
 
 % if less nuclei then empty patch, otherwise find features
-if length(nuclei_centroids_rounded) < 10
+if length(nuclei_centroids_rounded) < 5
     fprintf('Empty patch \n');
     features = zeros(892, 1);
-    til_mask = zeros(2000, 2000);
-    imwrite(til_mask, results_file);
 else
-    is_lymphocyte = (predict(til_model.model, nuclei_features(:,1:7))) == 1;
+    %is_lymphocyte = (predict(til_model.model, nuclei_features(:,1:7))) == 1;
     for c=1:length(nuclei_centroids_rounded)
         epi_nuclei(c) = epi_mask(nuclei_centroids_rounded(c, 2), nuclei_centroids_rounded(c, 1));
     end
 
-    % get epi-TILs, epi non-TILs, stroma TILs and stroma non-TILs
-    %coords = {nuclei_centroids_rounded(~is_lymphocyte & epi_nuclei,:), nuclei_centroids_rounded(is_lymphocyte & ~epi_nuclei,:), nuclei_centroids_rounded(is_lymphocyte & epi_nuclei,:), nuclei_centroids_rounded(~is_lymphocyte & ~epi_nuclei,:),};
-    %if (length(nuclei_centroids_rounded(~is_lymphocyte & epi_nuclei,:)) < 10) || (length(nuclei_centroids_rounded(is_lymphocyte & ~epi_nuclei,:)) < 10) || (length(nuclei_centroids_rounded(is_lymphocyte & epi_nuclei,:)) < 10) || (length(nuclei_centroids_rounded(~is_lymphocyte & ~epi_nuclei,:)) < 10)
-    %    % not useful patch for feature extraction
-    %    fprintf("Not enough nuclei for extracting graph interplay features!");
-    %    features = zeros(892, 1);
-    %else
-    %    % extract graph interplay features
-    %    [features_all_together, all_descriptions] = extract_graph_interplay_features(coords);
-    %    features = features_all_together;
-    %end
-
-    %% draw centroids, graphs, convex hull for all families
-    til_mask = zeros(2000, 2000);
-    if length(epi_nuclei) > 0
-        if length(is_lymphocyte) > 0
-            for c=1:length(nuclei_centroids_rounded)
-                if is_lymphocyte(c)
-                    bbox = nuclei_bboxes(c, :);
-                    til_mask(bbox(2) : bbox(2) + bbox(4), bbox(1) : bbox(1) + bbox(3), :) = 1;
-                end 
-            end
-        end
+    for c=1:length(nuclei_centroids_rounded)
+        tam_nuclei(c) = tam_mask(nuclei_centroids_rounded(c, 2), nuclei_centroids_rounded(c, 1));
     end
-    imwrite(til_mask, results_file);
+
+    % get epi-TAMs, epi non-TAMs, stroma TAMs and stroma non-TAMs
+    coords = {nuclei_centroids_rounded(~tam_nuclei & epi_nuclei,:), nuclei_centroids_rounded(tam_nuclei & ~epi_nuclei,:), nuclei_centroids_rounded(tam_nuclei & epi_nuclei,:), nuclei_centroids_rounded(~tam_nuclei & ~epi_nuclei,:),};
+    if (length(nuclei_centroids_rounded(~tam_nuclei & epi_nuclei,:)) < 5) || (length(nuclei_centroids_rounded(tam_nuclei & ~epi_nuclei,:)) < 5) || (length(nuclei_centroids_rounded(tam_nuclei & epi_nuclei,:)) < 5) || (length(nuclei_centroids_rounded(~tam_nuclei & ~epi_nuclei,:)) < 5)
+        % not useful patch for feature extraction
+        fprintf("Not enough nuclei for extracting graph interplay features!");
+        features = zeros(892, 1);
+    else
+        % extract graph interplay features
+        [features_all_together, all_descriptions] = extract_graph_interplay_features(coords);
+        features = features_all_together;
+        flag = 1;
+    end
 
     if draw_option == 1
         classes = zeros(1, length(nuclei_centroids_rounded));
--- code/main_1.m
+++ code/main_1.m
@@ -5,53 +5,58 @@ clear
 addpath(genpath('pwd'))
 
 %% hard-coded paths
-%patches_dir = "../../ovarian_cancer_results/patches_sample/";
-%patches = dir(fullfile(patches_dir, '*.png'));
-%epi_stroma_masks_dir = "../../ovarian_cancer_results/epi_stroma_masks_final/";
-%nuclei_masks_dir = "../../ovarian_cancer_results/nuclei_masks_final/";
-%histoqc_masks_dir = "";
-%results_images_dir = "../../ovarian_cancer_results/til_masks_sample/";
-%results_features_dir = "./";
-%til_model_path = "../../ovarian_cancer_model_files/lymp_svm_matlab_wsi.mat";
-%draw_option = 0;
+patches_dir = "../../yale_lung_cancer/patches/";
+patches = dir(fullfile(patches_dir, '*.png'));
+epi_stroma_masks_dir = "../../yale_lung_cancer/epi_stroma_masks/";
+nuclei_masks_dir = "../../yale_lung_cancer/nuclei_masks/";
+histoqc_masks_dir = "";
+tam_masks_dir = "../../yale_lung_cancer/macrophage_masks_final/";
+results_images_dir = "../../ovarian_cancer_results/til_masks_sample/";
+results_features_dir = "../../yale_lung_cancer/macrophage_output_spatial/";
+til_model_path = "../../ovarian_cancer_model_files/lymp_svm_matlab_wsi.mat";
+draw_option = 0;
 
 %% hard-coded paths on HPC
-patches_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/macrophage_patches/patches_HE/";
-patches = dir(fullfile(patches_dir, '*.png'));
+%patches_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/macrophage_patches/patches_HE/";
+%patches = dir(fullfile(patches_dir, '*.png'));
 %epi_stroma_masks_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_cancer/epi_stroma_masks/";
-nuclei_masks_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/macrophage_patches/nuclei_masks/";
+%nuclei_masks_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/macrophage_patches/nuclei_masks/";
 %histoqc_masks_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_cancer/histoqc_masks/";
-results_images_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/macrophage_patches/til_masks/";
+%results_images_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/macrophage_patches/til_masks/";
 %results_features_dir = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/tcga_ovarian_cancer/results_new/features/";
-til_model_path = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/til_biomarker_ovarian_cancer/model_files/lymp_svm_matlab_wsi.mat";
-draw_option = 0;
+%til_model_path = "/mnt/rstor/CSE_BME_AXM788/home/axa1399/til_biomarker_ovarian_cancer/model_files/lymp_svm_matlab_wsi.mat";
+%draw_option = 0;
 
 
 %% get til features
-for index = 1:500
+for index = 1:1000
     filename = patches(index).name;
-    %epi_stroma_mask_path = epi_stroma_masks_dir + filename;
+    filename
+    epi_stroma_mask_path = epi_stroma_masks_dir + filename;
     nuclei_mask_path = nuclei_masks_dir + filename;
+    tam_mask_path = tam_masks_dir + filename;
     %histoqc_mask_path = histoqc_masks_dir + filename;
     results_image_path = char(results_images_dir + filename);
 
     if isfile(nuclei_mask_path)
         %% read patches, epi/stroma mask, nuclei mask, HistoQC mask, TIL model
         image = im2double(imread(patches_dir + filename));
-        %epi_stroma_mask = im2double(imread(epi_stroma_mask_path));
-        epi_stroma_mask = ones(2000, 2000);
+        epi_stroma_mask = im2double(imread(epi_stroma_mask_path));
         nuclei_mask = im2double(imread(nuclei_mask_path));
-        nuclei_mask = nuclei_mask(1:2000, 1:2000);
+        tam_mask = im2double(imread(tam_mask_path));
         %histoqc_mask = im2double(imread(histoqc_mask_path));
-        histoqc_mask = ones(2000, 2000);
+        histoqc_mask = ones(500, 500);
         epi_mask = (epi_stroma_mask.*histoqc_mask);
         stroma_mask = ((1-epi_stroma_mask).*histoqc_mask);
         nuclei_mask = (nuclei_mask.*histoqc_mask);
+        tam_mask = (tam_mask.*histoqc_mask);
         til_model = load(til_model_path);
 
         %% run til pipeline
-        [features] = extract_til_features(image, nuclei_mask, histoqc_mask, epi_mask, stroma_mask, til_model, draw_option, results_image_path);
-        %filename = extractBefore(filename, ".png");
-        %writematrix(features, results_features_dir + filename + '.csv');
+        [features, flag] = extract_til_features(image, nuclei_mask, histoqc_mask, epi_mask, stroma_mask, til_model, draw_option, results_image_path, tam_mask);
+        if flag == 1
+            filename = extractBefore(filename, ".png");
+            writematrix(features, results_features_dir + filename + '.csv');
+        end
     end
 end
